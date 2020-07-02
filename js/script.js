76ece643ce5aec96f1c0f01d53c1eaa02839c78f
Alan Marcinkiewicz Mac <marcinkiewicz.alan@gmail.com>
2020-07-02 18:04:24 +0200

    Add playerMove

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// COMPUTER MOVE //
+
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
@@ -9,7 +11,25 @@ if(randomNumber == 1){
 } else if(randomNumber == 2){
   computerMove = 'papier';
 } else if(randomNumber == 3){
-  computerMove = 'nożyczki';
+  computerMove = 'nożyce';
 }
 
 printMessage('Mój ruch to: ' + computerMove);
+
+// PLAYER MOVE //
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+} else if(playerInput == '2'){
+  playerMove = 'papier';
+} else if(playerInput == '3'){
+  playerMove = 'nożyce';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
